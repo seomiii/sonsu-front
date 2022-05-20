bab8bcc8596f625f47e06358b81e0b074bc1e6c2
JangJiHyeon <76553061+jihyeon9975@users.noreply.github.com>
2022-05-21 02:24:20 +0900

    Create TestPopup.js by jjh

---
+++ sonsu_web/src/component/TestPopup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TestPopup extends React.Component{
+
+    render(){
+        return (
+            <div>
+                <h1>팝업 납시오</h1>
+                <h1>짜잔</h1>
+            </div>
+        );
+    }
+}
+
+export default TestPopup;
