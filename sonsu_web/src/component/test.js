29f86fc004739bfaf1fa15fdc43ebeb486daa25b
JangJiHyeon <76553061+jihyeon9975@users.noreply.github.com>
2022-05-21 02:22:03 +0900

    Update Test.js by jjh

--- sonsu_web/src/component/test.js
+++ sonsu_web/src/component/test.js
@@ -1,1 +1,51 @@
-// test_jjh
+import React from 'react';
+import { Paper, Grid, Button } from "@material-ui/core";
+
+
+class Test extends React.Component{
+
+    onButtonClick = () => {
+        window.open('TestPopup.js',
+        'TestPopup', 'width=300, height=300, menubar=no, status=no, toolbar=no'); 
+      }
+
+    render(){
+        return (
+            <Paper style={{ margin: 16, padding: 16 }}>
+                <Grid container>
+                    {/* <Header /> */}
+                    <Grid xs={10} md={10} item style={{ paddingRight: 16 }}>
+                        <Button 
+                            color="secondary"
+                            variant="contained"
+                            onClick={this.onButtonClick}>
+                            초급
+                        </Button>
+                    </Grid>
+
+
+                    <Grid xs={10} md={10} item style={{ paddingRight: 16 }}>
+                        <Button 
+                            color="secondary"
+                            variant="contained"
+                            onClick={this.onButtonClick}>
+                            중급
+                        </Button>
+                    </Grid>
+
+                    <Grid xs={10} md={10} item style={{ paddingRight: 16 }}>
+                        <Button 
+                            color="secondary"
+                            variant="contained"
+                            onClick={this.onButtonClick}>
+                            고급
+                        </Button>
+                    </Grid>
+                    {/* <Footer / > */}
+                </Grid>
+            </Paper>
+        );
+    }
+}
+
+export default Test;
