5db1b887575f9c41deae7661d56fe5c09a67735d
JangJiHyeon <76553061+jihyeon9975@users.noreply.github.com>
2022-05-21 02:22:48 +0900

    Update App.js for test by jjh

--- sonsu_web/src/App.js
+++ sonsu_web/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from './component/Test';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          {/* 지현이의 테스트입니다. */}
-          {/* 정윤이의 테스트입니다. */}
-          
-        </a>
+        <Test/>
       </header>
     </div>
   );
