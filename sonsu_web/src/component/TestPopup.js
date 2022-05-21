3f7b26b1d646b80238285ebbc6a1c28b3f5c0714
JangJiHyeon <76553061+jihyeon9975@users.noreply.github.com>
2022-05-22 02:06:54 +0900

    Update TestPopup by jjh

--- sonsu_web/src/component/TestPopup.js
+++ sonsu_web/src/component/TestPopup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 import '../component_css/TestPopup.css';
 
 class TestPopup extends React.Component{
@@ -19,8 +20,15 @@ function Testdetail(){
             {/* 동영상 팝업 */}
             {/* 이동 버튼 */}
             <h1>
-                동영상 팝업입니다.
+                동영상 팝업입니다. 우선 시범적으로 유튜브 영상을 넣었습니다.
             </h1>
+            <ReactPlayer
+                    className='reactplayer'
+                    url={'https://youtu.be/BjOPmIcC_fo'}    // 플레이어 url
+                    width='100%'         // 플레이어 크기 (가로)
+                    height='1000px'        // 플레이어 크기 (세로)
+                    controls={true}       // 플레이어 컨트롤 노출 여부
+                />
         </div>
     );
 }
