4a454a7967c1f412a428d66c916dd2c1c6ec1505
seomiii <rlaaltj1765@gmail.com>
2022-05-27 10:00:46 +0900

    1st complete study.js

--- sonsu_web/src/App.js
+++ sonsu_web/src/App.js
@@ -4,26 +4,11 @@ import Study from './component/Study';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          {/* 지현이의 테스트입니다. */}
-          {/* 정윤이의 테스트입니다. */}
-
-          <Study />
-          
-        </a>
-      </header>
+    <div className="App"> 
+      <p>~sonsu~</p>    
+      {/* <Study />         */}
+       
+     
     </div>
   );
 }
