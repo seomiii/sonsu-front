36d8ae385e6d9f2c39d5134657423dc7188a0917
jihyeonjjang <jihyeon9975@gmail.com>
2022-07-12 14:17:34 +0900

    update TestHome.js by jjh

--- sonsu_web/src/component/TestHome.js
+++ sonsu_web/src/component/TestHome.js
@@ -1,48 +1,49 @@
+import '../component_css/TestHome.css';
 import React from 'react';
-import { Paper, Grid, Button } from "@material-ui/core";
+import { Grid } from '@material-ui/core';
 import { Link } from "react-router-dom";
+import imgcho from './images/cho.png';
+import imgjung from './images/jung.png';
+import imggo from './images/go.png';
+
 
 class TestHome extends React.Component{
 
     render(){
         return (
-            <Paper style={{ margin: 16, padding: 16 }}>
-                <Grid container>
-                    <Grid xs={10} md={10} item style={{ paddingRight: 16 }}>
+            <div>
+                <Grid container spacing={3} alignItems="center" justifyContent="center">
+                    <Grid item xs={2}>
+                        <img src={imgcho} width={200} height={200} alt="cho"/> <br/>
+                        <h3>초급</h3>
+                        <label>초급에서 배운 수어의 지문자를 테스트 해봅시다.</label> <br/> <br/>
                         <Link to={"/test"}
                             state={{grade : 'cho'}}>
-                            <Button 
-                                color="secondary"
-                                variant="contained">
-                                초급
-                            </Button>
+                            <button className="start">시작하기</button>
                         </Link>
                     </Grid>
                     
-
-                    <Grid xs={10} md={10} item style={{ paddingRight: 16 }}>
+                    <Grid item xs={2}>
+                        <img src={imgjung} width={200} height={200} alt="cho"/> <br/>
+                        <h3>중급</h3>
+                        <label>중급에서 배운 일상생활에서 자주 쓰이는 표현들을 테스트 해봅시다.</label> <br/> <br/>
                         <Link to={"/test"}
                             state={{grade : 'jung'}}>
-                            <Button 
-                                color="secondary"
-                                variant="contained">
-                                중급
-                            </Button>
+                            <button className="start">시작하기</button>
                         </Link>
                     </Grid>
 
-                    <Grid xs={10} md={10} item style={{ paddingRight: 16 }}>
+                    <Grid item xs={2}>
+                        <img src={imggo} width={200} height={200} alt="cho"/> <br/>
+                        <h3>고급</h3>
+                        <label>고급에서 배운 다양한 단어들을 테스트 해봅시다.</label> <br/> <br/>
                         <Link to={"/test"}
                             state={{grade : 'go'}}>
-                            <Button 
-                                color="secondary"
-                                variant="contained">
-                                고급
-                            </Button>
+                            <button className="start">시작하기</button>
                         </Link>
                     </Grid>
                 </Grid>
-            </Paper>
+            </div>
         );
     }
 }
