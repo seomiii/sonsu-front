98b34081bd2336c5b1fddfbae8772a4e4e6fdb83
jihyeonjjang <jihyeon9975@gmail.com>
2022-07-12 16:40:18 +0900

    update test.js by jjh 0712

--- sonsu_web/src/component/Test.js
+++ sonsu_web/src/component/Test.js
@@ -1,6 +1,7 @@
-import '../component_css/TestPopup.css';
+import '../component_css/Test.css';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
+import { useLocation, Link } from 'react-router-dom';
 import Webcam from "react-webcam";
 
 class TestPopup extends React.Component{
@@ -8,25 +9,50 @@ class TestPopup extends React.Component{
     render(){
         return (
             <div className='TestPopup'>
-                <Testdetail />
-                <Testsidebar />
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Testheader />
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Testdetail />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Testsidebar />
+                    </Grid>
+                </Grid>
+                {/* <Testsidebar /> */}
             </div>
         );
     }
 }
 
+function Testheader(){
+    return(
+        <div style={{marginLeft: 20}} className="Testheader" align="left" >
+            <Link to={"/test_home"}>
+                <button className='back'>&lt; &nbsp; 돌아가기</button>
+            </Link>
+            &emsp; 초급
+        </div>
+    )
+}
+
 function Testdetail(){
     return(
-        <div className="Testdetail">
+        <div className="Testdetail" align="center">
             <Webcam
                 audio={false}
                 mirrored={true}
                 // height={720}
                 // width={1280}
-                height={500}
-                width={900}
-                padding={100}
+                height={600}
+                width={700}
+                // padding={100}
             ></Webcam>
+            <br/>
+            <button className='webcam'> 촬영 시작 </button>
+            &emsp;
+            <button className='webcam'> 결과 보기 </button>
         </div>
     );
 }
@@ -43,15 +69,14 @@ function Testsidebar(){
                     go : <h3 className='title_'>고급</h3>,
                 }[chapter]
             }
-            <ul>                
-                <ol type="1">
-                    <li className='question_'>1번 문제</li>
-                    <li className='question'>2번 문제</li>
-                    <li className='question'>3번 문제</li> 
-                    <li className='question'>4번 문제</li>
-                    <li className='question'>5번 문제</li>    
-                </ol>
-            </ul>
+                <li className='question_'>1번 문제</li>
+                <li className='question'>2번 문제</li>
+                <li className='question'>3번 문제</li> 
+                <li className='question'>4번 문제</li>
+                <li className='question'>5번 문제</li>
+            <br/>
+
+            <button className='webcam'>이전 문제로</button><button className='webcam'>다음 문제로</button>
         </div>
     );
 }
