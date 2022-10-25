8f2ea2b43559bf6321e6e24e268e743a4d983be7
jeong-yooon <chjy1819@gmail.com>
2022-10-25 15:10:55 +0900

    1025 yoon2

--- sonsu_web/src/component/grade.js
+++ sonsu_web/src/component/grade.js
@@ -8,6 +8,15 @@ import axios from 'axios';
 import Header from './header'
 import { Myheader } from './../component_css/Mypage_style';
 import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+  } from "recharts";
+import {
     GradeDiv,
     GradeTitle,
     GradeText,
@@ -48,9 +57,83 @@ const Grade = (props) => {
 
     const level_name = Level_Arr.curri_arr;
 
+    const [lastIdxA, setLastIdxA] = useState(0);
+    const [lastIdxB, setLastIdxB] = useState(0);
+    const [lastIdxC, setLastIdxC] = useState(0);
+    const [inputData, setInputData] = useState([]);
     const [month,setMonth]=useState(todayMonth);
     const [data,setData]=useState([]);
 
+    const [testData, setTestData] = useState([]);
+
+    // 그래프 데이터
+    useEffect(async()=>{
+        try{
+
+        const res = await axios.get(`/test/${year}/10/1`)
+        // console.log('response: ', res.data.data);
+
+        // const test = await res.data.data.level1.map()
+        // setTestData(testData.concat(test))
+        // setTestData(res.data.data);
+
+        const _inputDataA = await res.data.data.level1.map((rowData) => (
+            setLastIdxA(lastIdxA+1),
+            {
+            testDate: (rowData.testDate).slice(8,10),
+            averageA: (rowData.average)
+            
+            }
+        ))
+        const _inputDataB = await res.data.data.level2.map((rowData) => (
+            setLastIdxB(lastIdxB+1),
+            {
+            testDate: (rowData.testDate).slice(8,10),
+            averageB: (rowData.average)
+            
+            }
+        ))
+        const _inputDataC = await res.data.data.level3.map((rowData) => (
+            setLastIdxC(lastIdxC+1),
+            {
+            testDate: (rowData.testDate).slice(8,10),
+            averageC: (rowData.average)
+            
+            }
+        ))
+
+        setInputData(inputData.concat(_inputDataA, _inputDataB, _inputDataC))   
+            
+
+        }
+        catch(e){
+        console.error(e.message)
+        }
+    },[])
+
+    console.log('testdata: ', testData);
+    console.log('App :: inputData :: ', inputData)
+
+    useEffect(()=>{
+        const graphData=inputData.map((i)=>(
+          {
+            // name: `${i.testDate}일`,
+            name : i.testDate,
+            초급맞춘갯수 : i.averageA,
+            중급맞춘갯수 : i.averageB, 
+            고급맞춘갯수 : i.averageC, 
+          }
+        ))
+        setData(data.concat(graphData)) 
+    
+        // const add={name:'1', 갯수:1};
+        // setData(data.concat(add));
+    
+      },[inputData])
+    
+      console.log(data) 
+
+    // 성적표 자체 데이터
     useEffect(()=>{
         axios.get(`/note/${year}/${month}/1`)        
         .then((response)=>{
@@ -95,7 +178,35 @@ const Grade = (props) => {
                     <GradeChart>
                         <ChartDivA>
                             <ChartContent>초급</ChartContent>
-                            <Chart/>
+                            {/* <Chart/> */}
+                            <LineChart
+                                width={800}
+                                height={400}
+                                data={data}
+                                margin={{
+                                    top: 10,
+                                    right: 30,
+                                    left: 20,
+                                    bottom: 5
+                                }}
+                                //   lineSize={20}
+                                >
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    {/* <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} /> */}
+                                    <XAxis dataKey="name"/>
+                                    <YAxis />
+                                    <Tooltip />
+                                    <Legend />
+                                    {/* <CartesianGrid strokeDasharray="3 3"/> */}
+                                    <Line type="monotone" dataKey="초급맞춘갯수" stroke="black">
+                                    </Line>
+                                    <Line type="monotone" dataKey="중급맞춘갯수" stroke="#8884d8">
+                                    </Line>
+                                    <Line type="monotone" dataKey="고급맞춘갯수" stroke="#82ca9d">
+                                    </Line>
+                                    {/* <Line dataKey="맞춘갯수" fill="#FF7A00" background={{ fill: "#D9D9D9" }} /> */}
+
+                                </LineChart>
                         </ChartDivA>
                         {/* <ChartDivB>
                             <ChartContent>중급</ChartContent>
@@ -109,7 +220,7 @@ const Grade = (props) => {
                 </GradeDiv>
             </MediaDiv>
         </>
-    )
+    );
 }
 
 export default Grade
--- sonsu_web/src/component/mypage.js
+++ sonsu_web/src/component/mypage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { PieChart, Pie, Sector, Cell } from "recharts";
 import Chart from "./chart/mypage_chart";
 import axios from 'axios';
 import {useLocation, Link} from 'react-router-dom';
@@ -34,6 +35,8 @@ import {
 import { HeaderDiv } from '../component_css/Home_style';
 import userEvent from '@testing-library/user-event';
 
+const COLORS = ["#FF7A00", "#ffffff"];
+
 const Mypage = () => {
 
     const [UserData,setUserData] = useState();
@@ -53,6 +56,21 @@ const Mypage = () => {
     var level2_per = UserData && Math.round((UserData.progressDto[1].doneCount / UserData.progressDto[1].totalCount)*100)
     var level3_per = UserData && UserData && Math.round((UserData.progressDto[2].doneCount / UserData.progressDto[2].totalCount)*100)
     
+    const data1 = [
+        { name: "process", value: UserData && (UserData.progressDto[0].doneCount) },
+        { name: "n process", value: UserData && (UserData.progressDto[0].totalCount-UserData.progressDto[0].doneCount) },
+    ];
+    
+    const data2 = [
+        { name: "process", value: UserData && (UserData.progressDto[1].doneCount) },
+        { name: "n process", value: UserData && (UserData.progressDto[1].totalCount-UserData.progressDto[1].doneCount) },
+    ];
+    
+    const data3 = [
+        { name: "process", value: UserData && (UserData.progressDto[2].doneCount) },
+        { name: "n process", value: UserData && (UserData.progressDto[2].totalCount-UserData.progressDto[2].doneCount) },
+    ];
+
     return (
         <>            
             <HeaderDiv>
@@ -104,7 +122,59 @@ const Mypage = () => {
                                     </ProfileText>
                                 </Process>
                                 <ChartBox>
-                                    <Chart/>
+                                    {/* <Chart/> */}
+                                    <PieChart width={850} height={250}>
+                                        <Pie
+                                            data={data1}
+                                            cx={190}
+                                            cy={130}
+                                            innerRadius={50}
+                                            outerRadius={90}
+                                            fill="#8884d8"
+                                            stroke="#FF7A00"
+                                            strokeWidth={1}
+                                            paddingAngle={5}
+                                            dataKey="value"
+                                        >
+                                            {data1.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                            ))}
+                                        </Pie>
+
+                                        <Pie
+                                            data={data2}
+                                            cx={460}
+                                            cy={130}
+                                            innerRadius={50}
+                                            outerRadius={90}
+                                            fill="#8884d8"
+                                            stroke="#FF7A00"
+                                            strokeWidth={1}
+                                            paddingAngle={5}
+                                            dataKey="value"
+                                        >
+                                            {data2.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                            ))}
+                                        </Pie>
+
+                                        <Pie
+                                            data={data3}
+                                            cx={730}
+                                            cy={130}
+                                            innerRadius={50}
+                                            outerRadius={90}
+                                            fill="#8884d8"
+                                            stroke="#FF7A00"
+                                            strokeWidth={1}
+                                            paddingAngle={5}
+                                            dataKey="value"
+                                        >
+                                            {data3.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                            ))}
+                                        </Pie>
+                                        </PieChart>
                                     <ChartName>
                                         <PieName> {level1_per} % </PieName>
                                         <PieName> {level2_per} % </PieName>
@@ -123,7 +193,7 @@ const Mypage = () => {
             </MediaDiv>
             {/* <Chart options={options} series={series} type="radialBar" height="50" /> */}
         </>       
-    )
+    );
 
 }
 
